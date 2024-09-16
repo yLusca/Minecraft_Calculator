23c9af5c7dccc24f9ca772cf2e7b175f1b642a29
yLusca <152924367+yLusca@users.noreply.github.com>
2024-09-16 18:34:33 -0300

    Subindo os primeiros arquivos
    Subindo os primeiros arquivos e script de teste da calculadora

---
+++ Projeto01.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+n1 = int(input('Digite o primeiro número '))
+n2 = int(input('Digite o segundo número '))
+s = n1+n2
+
+print('A soma entre {} e {} vale {}'.format(n1,n2,s))
+
+Tk()
